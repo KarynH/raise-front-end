dd5b4d044e2ae77cda5c04d7c11e6fd78d2126cc
Karyn Huston <huston.karyn@gmail.com>
2023-02-12 19:58:42 -0500

    index.css, nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,19 +1,26 @@
-import { Link } from "@mui/material"
+import { Link } from "@mui/material";
 
-import  Navbar  from "react-bootstrap/Navbar";
+import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 
+import Stack from "@mui/material/Stack";
+
 export default function Nav() {
-
   return (
-
-    <Navbar>
+    <Navbar className="nav">
       <Container>
-     <Link href="/questions" underline="none" variant="h2"    sx={{
-        fontFamily: 'Ramaraja',
-      }}>{'raise'}</Link>
+        <Link
+          href="/questions"
+          underline="none"
+          variant="h2"
+          sx={{
+            fontFamily: "Ramaraja",
+          }}
+        >
+          {"raise"}
+        </Link>
+        <Stack direction="row" spacing={2}></Stack>
       </Container>
     </Navbar>
-   
   );
 }
