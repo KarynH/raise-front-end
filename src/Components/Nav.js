b220551e69ef098bf27c7f6e95cbd2b22c65c587
Karyn Huston <huston.karyn@gmail.com>
2023-02-14 19:59:39 -0500

    revised css for nav, update index

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,19 +6,24 @@ import Col from "react-bootstrap/Col";
 
 export default function Nav() {
   return (
-    <nav className="nav">
-      {/* <div className="navContent">
+    /* 
+
+font-family: 'Playfair Display', serif;
+font-family: 'PT Serif', serif;
+font-family: 'Quicksand', sans-serif;
+ */
+    <nav>
       <Link
         className="home"
         href="/"
         underline="none"
         variant="h4"
         sx={{
-          fontFamily: "Ramaraja",
+          fontFamily: "Quicksand",
         }}
       >
         {" "}
-        <span className="material-symbols-outlined home">Home</span>{" "}
+        <span className="home">Home</span>{" "}
       </Link>
      
         <Link
@@ -36,61 +41,16 @@ export default function Nav() {
         className="parentingTips"
           href="/questions"
           underline="none"
-          variant="h4"
+          variant="h3"
           sx={{
             fontFamily: "Ramaraja",
           }}
         >
           {"parenting tips"}
         </Link>
-          <span className="material-symbols-outlined userLink">person</span>
-      </div> */}
-      <Container>
-        <Row>
-          <Col lg={1}>
-            <Link
-              className="raiseLogo"
-              href="/questions"
-              underline="none"
-              variant="h3"
-              sx={{
-                fontFamily: "Ramaraja",
-              }}
-            >
-              {"raise"}
-            </Link>
-            </Col>
-          <Col sm={2}>
-          <Link
-              className="raiseLogo"
-              href="/questions"
-              underline="none"
-              variant="h4"
-              sx={{
-                fontFamily: "Ramaraja",
-              }}
-            >
-              {"open discussions"}
-            </Link>
-          </Col>
-          <Col lg={4}>
-            <Link
-              className="parentingTips"
-              href="/questions"
-              underline="none"
-              variant="h4"
-              sx={{
-                fontFamily: "Ramaraja",
-              }}
-            >
-              {"parenting tips"}
-            </Link>
-          </Col>
-
-          <Col xs={1}><span className="material-symbols-outlined userLink">person</span>
-</Col>
-        </Row>
-      </Container>
-    </nav>
+          <span className="material-symbols-outlined userLink">{"login"}{"person"}</span>
+      </nav>
+     
+    
   );
 }
--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -3,27 +3,30 @@ import axios from "axios";
 
 import { useEffect, useState } from "react";
 
-
 export default function Questions() {
-
   const API = process.env.REACT_APP_API_URL;
   const [questions, setQuestions] = useState([]);
 
-    useEffect(() => {
-      axios.get(`${API}questions`).then((res) => {
-        console.log(res.data)
-        setQuestions(res.data)
-      }).catch((err) => console.err)
-    }, [API])
+  useEffect(() => {
+    axios
+      .get(`${API}questions`)
+      .then((res) => {
+        console.log(res.data);
+        setQuestions(res.data);
+      })
+      .catch((err) => console.err);
+  }, [API]);
 
   return (
- <>
- <h3 className="headingRecent">recent</h3>
- {
-  questions.map((question) => {
-    return <Question key={question.id} question={question} />
-  })
- }
- </>
+    <>
+      <h3 className="headingRecent">recent</h3>
+     
+       
+     
+
+      {questions.map((question) => {
+        return <Question key={question.id} question={question} />;
+      })}
+    </>
   );
 }
